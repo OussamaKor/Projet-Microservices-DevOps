d4df8ba62df1b26a7d2e70497ce4632997e17482
ahmed bahri <76647412+bahmed99@users.noreply.github.com>
2022-05-10 00:31:06 +0100

    Update index.js

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-
+const cors = require('cors')
 const app = express();
-
+app.use(cors())
 app.use(bodyParser.json());
 
 app.post("/events", (req, res) => {
